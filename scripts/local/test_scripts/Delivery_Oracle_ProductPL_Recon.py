fb4a13f12fa20ec3e41c8f6411f7047c16f66f35
gurudamodharan <96510967+gurudamodharan@users.noreply.github.com>
2022-03-21 20:21:20 +0530

    Ops Allocation Reconciliation ART
    Summary: These ARTs validate the data between Oracle and Product PL in Delivery and Mobility cubes post allocation to ensure 100 % of data is allocated. They are called as part of batches 1102 and 1103
    
    Test Plan: Attached in JIRA
    
    Reviewers: #tm1_application_platform_tap, #ldap_tm1, balajik
    
    Reviewed By: #tm1_application_platform_tap, #ldap_tm1, balajik
    
    Subscribers: #ldap_tm1
    
    JIRA Issues: FINTELPA-4168
    
    Differential Revision: https://code.uberinternal.com/D7305771

---
+++ scripts/local/test_scripts/Delivery_Oracle_ProductPL_Recon.py
@@ -0,0 +1,72 @@
+
+from datetime import datetime
+from tm1tests.reconciliation import Reconciliation
+
+source_mdx = """SELECT NON EMPTY
+    {{TM1FILTERBYLEVEL( {TM1DRILLDOWNMEMBER( {[Period].[%s]}, ALL, RECURSIVE )}, 0)} + {TM1FILTERBYLEVEL( {TM1DRILLDOWNMEMBER( {[Period].[%s]}, ALL, RECURSIVE )}, 0)}}
+    ON ROWS,
+    NON EMPTY
+    {[Delivery Ops Metrics].[Gross Bookings],[Delivery Ops Metrics].[Pricing, Incentives, and Other Revenue],[Delivery Ops Metrics].[Net Effective Take Rate (NETR)],[Delivery Ops Metrics].[Variable Costs],[Delivery Ops Metrics].[Variable Contribution],
+    [Delivery Ops Metrics].[Operating Expenses],[Delivery Ops Metrics].[Adj EBITDA]}
+    ON COLUMNS
+FROM
+    [Delivery]
+WHERE (
+    [Version].[Actual],
+    [Source].[Oracle],
+    [Rate Type].[USD],
+    [Line of Business].[Delivery],   
+    [Department].[Total Org],
+    [Delivery Fulfillment Type].[Total Fulfillment Type],
+    [Delivery Merchant Type].[Total Merchant Type],
+    [Delivery Order Category].[Total Order Category],
+    [Delivery Merchant Segment].[Total Merchant Segment],
+    [Delivery Features].[All Delivery Features],
+    [Location].[Total Location by Country],
+    [Delivery Measure].[Amount]
+)"""
+
+target_mdx = """SELECT NON EMPTY
+    {{TM1FILTERBYLEVEL( {TM1DRILLDOWNMEMBER( {[Period].[%s]}, ALL, RECURSIVE )}, 0)} + {TM1FILTERBYLEVEL( {TM1DRILLDOWNMEMBER( {[Period].[%s]}, ALL, RECURSIVE )}, 0)}}
+    ON ROWS,
+    NON EMPTY
+    {[Delivery Ops Metrics].[Gross Bookings],[Delivery Ops Metrics].[Pricing, Incentives, and Other Revenue],[Delivery Ops Metrics].[Net Effective Take Rate (NETR)],[Delivery Ops Metrics].[Variable Costs],[Delivery Ops Metrics].[Variable Contribution],
+    [Delivery Ops Metrics].[Operating Expenses],[Delivery Ops Metrics].[Adj EBITDA]}
+    ON COLUMNS
+FROM
+    [Delivery]
+WHERE (
+    [Version].[Actual],
+    [Source].[Product PL],
+    [Rate Type].[USD],
+    [Line of Business].[Delivery],   
+    [Department].[Total Org],
+    [Delivery Fulfillment Type].[Total Fulfillment Type],
+    [Delivery Merchant Type].[Total Merchant Type],
+    [Delivery Order Category].[Total Order Category],
+    [Delivery Merchant Segment].[Total Merchant Segment],
+    [Delivery Features].[All Delivery Features],
+    [Location].[Total Location by Country],
+    [Delivery Measure].[Amount]
+)"""
+
+class Mytest(Reconciliation):
+
+    name = 'Delivery Oracle Vs Product PL Reconciliation'
+    email_to = []
+    email_from = 'pa-eng@uber.com'
+    alert_level = {'email': 'error'}
+    source = [['ops', 'mdx', source_mdx]]
+    target = [['ops', 'mdx', target_mdx]]
+    schedule = '15 18 * * *'
+    keyword = ['Delivery']
+    threshold = ('ge', 1)
+
+
+    def prepare(self):
+        super().prepare()
+        now = datetime.now()
+        curr_yr = str(now.year)
+        prev_yr = str(now.year -1)
+        self.source[0][2] = source_mdx % (curr_yr,prev_yr)
+        self.target[0][2] = target_mdx % (curr_yr,prev_yr) 
---
+++ scripts/local/test_scripts/Mobility_Oracle_ProductPL_Recon.py
@@ -0,0 +1,71 @@
+
+from datetime import datetime
+from tm1tests.reconciliation import Reconciliation
+
+    
+source_mdx = """SELECT NON EMPTY
+    {{TM1FILTERBYLEVEL( {TM1DRILLDOWNMEMBER( {[Period].[%s]}, ALL, RECURSIVE )}, 0)} + {TM1FILTERBYLEVEL( {TM1DRILLDOWNMEMBER( {[Period].[%s]}, ALL, RECURSIVE )}, 0)}}
+    ON ROWS,
+    NON EMPTY
+    {[Mobility Ops Metrics].[Gross Bookings],[Mobility Ops Metrics].[Pricing, Incentives, and Other Revenue],[Mobility Ops Metrics].[Net Effective Take Rate (NETR)],[Mobility Ops Metrics].[Variable Costs],[Mobility Ops Metrics].[Variable Contribution],
+    [Mobility Ops Metrics].[Operating Expenses],[Mobility Ops Metrics].[Adj EBITDA]}
+    ON COLUMNS
+FROM
+    [Mobility]
+WHERE (
+    [Version].[Actual],
+    [Source].[Oracle],
+    [Rate Type].[USD],
+    [Line of Business].[Mobility],   
+    [Department].[Total Org],
+    [Product Type].[Total Product Type],
+    [Mobility Features].[All Mobility Features],
+    [Location].[Total Location Incl Discontinued],
+    [Mobility Measure].[Amount]
+)"""
+
+target_mdx = """SELECT NON EMPTY
+    {{TM1FILTERBYLEVEL( {TM1DRILLDOWNMEMBER( {[Period].[%s]}, ALL, RECURSIVE )}, 0)} + {TM1FILTERBYLEVEL( {TM1DRILLDOWNMEMBER( {[Period].[%s]}, ALL, RECURSIVE )}, 0)}}
+    ON ROWS,
+    NON EMPTY
+    {[Mobility Ops Metrics].[Gross Bookings],[Mobility Ops Metrics].[Pricing, Incentives, and Other Revenue],[Mobility Ops Metrics].[Net Effective Take Rate (NETR)],[Mobility Ops Metrics].[Variable Costs],[Mobility Ops Metrics].[Variable Contribution],
+    [Mobility Ops Metrics].[Operating Expenses],[Mobility Ops Metrics].[Adj EBITDA]}
+    ON COLUMNS
+FROM
+    [Mobility]
+WHERE (
+    [Version].[Actual],
+    [Source].[Product PL],
+    [Rate Type].[USD],
+    [Line of Business].[Mobility],   
+    [Department].[Total Org],
+    [Product Type].[Total Product Type],
+    [Mobility Features].[All Mobility Features],
+    [Location].[Total Location Incl Discontinued],
+    [Mobility Measure].[Amount]
+)"""
+
+class Mytest(Reconciliation):
+
+    name = 'Mobility Oracle Vs Product PL Reconciliation'
+    email_to = []
+    email_from = 'pa-eng@uber.com'
+    alert_level = {'email': 'error', 'page': 'critical'}
+    source = [['ops', 'mdx', source_mdx]]
+    target = [['ops', 'mdx', target_mdx]]
+    schedule = '15 18 * * *'
+    keyword = ['Mobility']
+    threshold = ('ge', 1)
+
+
+    def prepare(self):
+        super().prepare()
+        now = datetime.now()
+        curr_yr = str(now.year)
+        prev_yr = str(int(curr_yr) - 1)
+        self.source[0][2] = source_mdx % (curr_yr,prev_yr)
+        self.target[0][2] = target_mdx % (curr_yr,prev_yr)     
+
+
+
+        
