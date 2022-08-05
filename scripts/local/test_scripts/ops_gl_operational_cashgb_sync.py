312e919a0ac987d202907fe39bcaac1c3406e4ab
Niranjan <niranjan.prasad@uber.com>
2022-08-05 22:37:30 +0530

    Setting up Arts for Actuals data recon between Ops and Analytics for below metrics Cash GB - Seup as part of Batch - 1104 Cash Trips - Setup as part of Batch - 1015
    Summary: Cash GB and Cash Trips Actuals data recon between Ops and Analytics
    
    Test Plan: https://docs.google.com/document/d/1N146Fx9C_lr8lLrpd6CCsrcyMA7wx-29V_iIqeull_A/
    
    Reviewers: #tm1_application_platform_tap, #ldap_tm1, balajik
    
    Reviewed By: #tm1_application_platform_tap, #ldap_tm1, balajik
    
    Subscribers: balajik, #ldap_tm1
    
    JIRA Issues: FINTELPA-4957
    
    Differential Revision: https://code.uberinternal.com/D7976857

---
+++ scripts/local/test_scripts/ops_gl_operational_cashgb_sync.py
@@ -0,0 +1,59 @@
+
+from datetime import datetime
+from tm1tests.reconciliation import Reconciliation
+
+source_mdx = """SELECT NON EMPTY
+    {[Ops Metric].[Cash Gross Bookings]}
+    ON ROWS,
+    {TM1FILTERBYLEVEL({TM1DRILLDOWNMEMBER({[Period].[%s]},ALL,RECURSIVE)},0)}
+    ON COLUMNS
+FROM
+    [Ops]
+WHERE (
+    [Version].[Actual],
+    [Source].[FDS Incl. Adj],
+    [Line of Business].[Total Line of Business],
+    [Location].[Total Location Incl Discontinued],
+    [Product Type].[Total Product Type],
+    [Rate Type].[USD],
+    [Ops Measure].[Amount]
+)
+"""
+
+target_mdx = """SELECT NON EMPTY
+    {[Account].[Cash Gross Bookings]}
+    ON ROWS,
+    {TM1FILTERBYLEVEL({TM1DRILLDOWNMEMBER({[Month].[%s]},ALL,RECURSIVE)},0)}
+    ON COLUMNS
+FROM
+    [GL Operational]
+WHERE (
+    [Version].[Actual],
+    [Source].[OPS],
+    [Rate Type].[USD],
+    [Line of Business].[Total Line of Business],
+    [Location].[Total Location Incl Discontinued],
+    [Department].[Total Department],   
+    [Product Type].[Total Product Type],
+    [GL Operational].[GL Operational Stat Accounts],
+    [GL Operational Measure].[Amount]
+)
+"""
+
+class Mytest(Reconciliation):
+
+    name = 'GL Operational Cash Gross Bookings Actuals Recon'
+    email_to = []
+    email_from = 'pa-eng@uber.com'
+    alert_level = {'email': 'error'}
+    source = [['ops', 'mdx', source_mdx]]
+    target = [['analytics', 'mdx', target_mdx]]
+    threshold = ('ge', 1)
+
+    def prepare(self):
+        super().prepare()
+        session = self.apps_sessions['analytics']
+        current_actual_month = session.cubes.cells.get_value('System Info','Current Month, String')
+        year = current_actual_month[:4]        
+        self.source[0][2] =  source_mdx % (year)
+        self.target[0][2] =  target_mdx % (year)
