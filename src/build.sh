7a6efc24f5cabd133340b5e06c7a317489ac4cd9
Ramakrishnan Selvam <ramakrishnan@uber.com>
2024-07-18 15:21:55 +0530

    uncomment the call to TI which removes redundant objects from the ops application in gcp_stage
    Summary: Uncomment the call to TI which removes redundant objects from the ops application during the build stage in gcp_stage repo
    
    Reviewers: #tm1_application_platform_tap, #ldap_tm1, atul.awnish
    
    Reviewed By: #tm1_application_platform_tap, #ldap_tm1, atul.awnish
    
    Subscribers: #ldap_tm1
    
    JIRA Issues: FINTELPA-9530
    
    Differential Revision: https://code.uberinternal.com/D14426143

--- src/build.sh
+++ src/build.sh
@@ -10,7 +10,7 @@ shell_build1 () {
   #find . -name connect.json -type f -exec sed -i 's/tm1dev.awscorp.uberinternal.com/test.tm1ext.awscorp.uberinternal.com/g' {} \;
   #find . -name connect.json -type f -exec sed -i 's/tm1uat.awscorp.uberinternal.com/uat.tm1ext.awscorp.uberinternal.com/g' {} \;
   #find . -name connect.json -type f -exec sed -i 's/tm1.awscorp.uberinternal.com/prod.tm1ext.awscorp.uberinternal.com/g' {} \;
-  
+
   cat <<EOF > config/default/credentials.json
                 {
     "namespace": "TM1_NAMESPACE",
@@ -48,20 +48,20 @@ shell_build3 () {
 execute_tm1cm () {
   echo "Start tm1cm .."
 
-  
+
   ## Uncomment below for the connectivity test
   # curl -Ivk https://pac.uat.tm1ext.awscorp.uberinternal.com:443/api/v1/Cubes --connect-timeout 30
-  ## 
+  ##
 
   /work/.local/bin/python3.6 -m tm1cm --mode put --environment ${TARGET_ENVIRONMENT} --path $(pwd)
 
   echo "End tm1cm .."
 
-  # execute TI 
+  # execute TI
   execute_ti
-  
+
   echo "End TI execution .."
-  
+
 }
 
 execute_tm1cm_replica() {
@@ -69,9 +69,9 @@ execute_tm1cm_replica() {
   repo_name=$(basename $(git remote get-url origin))
 
   if [ -s "$replica_connect_file" ]
-  then 
+  then
     echo "$replica_connect_file file exists and is not empty. Proceeding ... "
-  
+
     ## connect and replica-connect for the target env
     connect_file=$(find ./config/${TARGET_ENVIRONMENT} -type f -name connect.json)
 
@@ -85,7 +85,7 @@ execute_tm1cm_replica() {
     for i in $(seq 0 $(( $len - 1 ))); do
         ## Take backup of the connect.json
         cp  ${connect_file} ${connect_file}.$i
-        
+
 	      ## extract the replica json content
         echo "Connect json for Replica $(( $i + 1 )) is $(cat $replica_connect_file| jq .[$i])"
         extract_replica_address="cat $replica_connect_file| jq .[$i]"
@@ -103,7 +103,7 @@ EOF
 
         ## Find and replace the primary instance name with replica name in the ART python scripts
         # echo "Find and Replace $repo_name with $extract_replica_name in ART Scripts"
-        
+
         # s1="source = \[\["\'$repo_name\'
         # s2="source = \[\["\'$extract_replica_name\'
         # find scripts/local/test_scripts -type f -name "*.py" -exec sed -i "s#$s1#$s2#g" {} \;
@@ -118,13 +118,13 @@ EOF
 
         # grep "source = " scripts/local/test_scripts/*.py
         # grep "target = " scripts/local/test_scripts/*.py
-        
+
         repo_name=$extract_replica_name
 
         ## Run tm1cm command on the new connect.json file created
         shell_build1
-        execute_tm1cm 
-        
+        execute_tm1cm
+
     done
   else
     echo "$replica_connect_file file does not exist, or is empty "
@@ -141,8 +141,8 @@ execute_ti () {
   address=$(cat config/${TARGET_ENVIRONMENT}/connect.json | jq -r '.address')
   echo $address
 
-  # curl -kv --location --request POST "https://$address:443/api/v1/Processes('TAP.Call.Export Redudant Objects')/ibm.tm1.api.v1.Execute" \
-  # --header "Authorization: CAMNamespace $auth_encode_base64" --header 'Content-Type: application/json; charset=utf-8' 
+  curl -kv --location --request POST "https://$address:443/api/v1/Processes('TAP.Call.Export%20Redudant%20Objects')/ibm.tm1.api.v1.Execute" \
+  --header "Authorization: CAMNamespace $auth_encode_base64" --header 'Content-Type: application/json; charset=utf-8'
 
   echo "TI execution skipped temporarily..."
 }
