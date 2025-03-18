7a80d12112bf093027b03b00a6c068a826086b54
Rajesh <rajesht@uber.com>
2025-03-18 15:14:52 +0530

    UAT cellar changes for data move code
    Test Plan:
    {F1051487863}
    
    {F1051487953}
    
    Reviewers: #tm1_application_platform_tap, #ldap_tm1, niranjan.prasad
    
    Reviewed By: #tm1_application_platform_tap, #ldap_tm1, niranjan.prasad
    
    Subscribers: #ldap_tm1
    
    Tags: #has_python
    
    JIRA Issues: FINTELPA-10490
    
    Differential Revision: https://code.uberinternal.com/D16848375

--- scripts/local/data-move.py
+++ scripts/local/data-move.py
@@ -4,15 +4,24 @@ import logging
 from taptools import configure_logging
 from taptools import send_alert
 from TM1py.Services import TM1Service
+from taptools import getsecret
+import sys
+import json
 
+
+app = sys.argv[0].split('/')[3]
+CONFIG_PATH = f'/var/tm1/{app}/config/deploy/node.json'
+ENVIRONMENT_CONFIG = '/opt/uber/environment.json'
+
+def get_environment():
+    with open(ENVIRONMENT_CONFIG, 'r') as f:
+        return json.load(f)
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--server', help='Server Name', required=False, default='ops.findev.uberinternal.com')
     parser.add_argument('--port', help='Server Port', required=False, default='443')
-    parser.add_argument('--user', help='Username', required=True)
     parser.add_argument('--ssl', help='SSL', required=False, default=True, type=bool)
-    parser.add_argument('--password', help='Password', required=True)
     parser.add_argument('--namespace', help='CAMNamespace', required=False, default='uberAD')
     parser.add_argument('--pCube', help='', required=False, default='OPS Monthly')
     parser.add_argument('--pView', help='', required=False, default='')
@@ -28,6 +37,15 @@ def main():
     parser.add_argument('--pDebug', help='', required=False, default=0)
 
     args = parser.parse_args()
+
+    node = json.load(open(CONFIG_PATH))
+    env = get_environment()
+    user = node['normal']['tm1']['user']
+    password = getsecret.get_password('tm1_password', env)
+
+    #Adding derived arguments from Cellar
+    args.user=user
+    args.password=password
 
     configure_logging(app='ops', debug=bool(args.pDebug))
 
