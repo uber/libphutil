bcdffb9096c376d8e440e388a85e4c3d68ee9509
Rajesh <rajesht@uber.com>
2025-03-24 14:50:37 +0530

    UAT cellar changes for generic process call
    Test Plan: {F1054691181}
    
    Reviewers: #tm1_application_platform_tap, #ldap_tm1, atul.awnish
    
    Reviewed By: #tm1_application_platform_tap, #ldap_tm1, atul.awnish
    
    Subscribers: #ldap_tm1
    
    Tags: #has_python
    
    JIRA Issues: FINTELPA-10490
    
    Differential Revision: https://code.uberinternal.com/D16916605

--- scripts/local/generic_process_call.py
+++ scripts/local/generic_process_call.py
@@ -3,24 +3,46 @@ import os
 import tempfile
 import logging
 import json
+import logging
 
 from TM1py.Services import TM1Service
 from base64 import urlsafe_b64decode, urlsafe_b64encode
 from taptools import configure_logging
 from taptools import send_alert
+from taptools import getsecret
+import sys
+
+app = sys.argv[0].split('/')[3]
+CONFIG_PATH = f'/var/tm1/{app}/config/deploy/node.json'
+ENVIRONMENT_CONFIG = '/opt/uber/environment.json'
+
+def get_environment():
+    with open(ENVIRONMENT_CONFIG, 'r') as f:
+        return json.load(f)
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--server', help='Server Name', required=False, default='localhost')
     parser.add_argument('--port', help='Server Port', required=False, default='20000')
-    parser.add_argument('--user', help='Username', required=True)
     parser.add_argument('--ssl', help='SSL', required=False, default=True, type=bool)
-    parser.add_argument('--password', help='Password', required=True)
     parser.add_argument('--namespace', help='CAMNamespace', required=True)
     parser.add_argument('--process', help='Process', required=True)
     parser.add_argument('--pLogPath', help='', required=True)
     parser.add_argument('--parameter_dict', help='Dictionary', required=True, type=str)
+    parser.add_argument('--pDebug', help='', required=False, default=0)
     args = parser.parse_args()
+
+    node = json.load(open(CONFIG_PATH))
+    env = get_environment()
+    user = node['normal']['tm1']['user']
+    password = getsecret.get_password('tm1_password', env)
+    #Adding derived arguments from Cellar
+    args.user=user
+    args.password=password
+    configure_logging(app='ops', debug=bool(args.pDebug))
+
+    logging.info('Called with parameters {}'.format(vars(args)))
+
     try:
         tm1_service = TM1Service(address=args.server, port=args.port, user=args.user, password=args.password, namespace=args.namespace, ssl=args.ssl)
         data=json.loads(args.parameter_dict)
